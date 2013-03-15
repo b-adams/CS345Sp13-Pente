ce622c13dce2ce5c1cb7273feaa2c284f8e99f34
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-03-15 13:11:10 -0400

    Location TDD: RED testNewLocationHasNegativeCoords

---
+++ CS345 Pente/CS345 PenteTests/TestLocation.m
@@ -0,0 +1,63 @@
+/**
+ @file TestLocation.m
+ @brief Part of CS345 Pente
+ @author Bryant Adams
+ @date 3/15/13
+ @copyright GNU Public License v3 or later, 2013
+ @version 0.1
+*/
+
+    // Class under test
+#import "CSPLocation.h"
+#import "CSPCoordinateInterface.h"
+
+    // Collaborators
+
+    // Test support
+#import <SenTestingKit/SenTestingKit.h>
+
+// Comment the next two lines to disable OCHamcrest (for test assertions)
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+// Comment the next two lines to disable OCMockito (for mock objects)
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
+/**
+ @brief For testing CSPLocation
+ */
+@interface TestLocation : SenTestCase
+@end
+
+@implementation TestLocation
+{
+    id<CSPCoordinateInterface> sut;
+}
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testNewLocationHasNegativeCoords
+{
+    // given
+    sut = [[CSPLocation alloc] init];
+    
+    // when
+    int x = [sut x];
+    int y = [sut y];
+    
+    // then
+    assertThatInt(x, is(@-1));
+    assertThatInt(y, is(@-1));
+}
+
+
+@end
