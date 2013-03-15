6d6f728700a0b38799837029a72a7c2231d88bd6
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-03-15 19:13:11 -0400

    Location TDD: Add direction enumeration to coordinates

--- CS345 Pente/CS345 Pente/CSPCoordinateInterface.h
+++ CS345 Pente/CS345 Pente/CSPCoordinateInterface.h
@@ -7,6 +7,18 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    CSDID_North,
+    CSDID_NorthEast,
+    CSDID_East,
+    CSDID_SouthEast,
+    CSDID_South,
+    CSDID_SouthWest,
+    CSDID_West,
+    CSDID_NorthWest,
+    CSDID_NONE
+} CSPDirectionID;
+
 @protocol CSPCoordinateInterface <NSObject>
 
 @property (readonly, assign) int x;
