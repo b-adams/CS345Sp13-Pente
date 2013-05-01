f132f6683ef2b6813e37063a9713117afd9f7a57
Josh Brutscher <jbrutscher@Mac110-Golgari.local>
2013-05-01 10:59:37 -0400

    committing working version.

--- CS345 Pente/CS345 Pente/CSPBoardView.m
+++ CS345 Pente/CS345 Pente/CSPBoardView.m
@@ -8,12 +8,12 @@
 
 #import "CSPBoardView.h"
 #import "CSPBump.h"
-#import "CSPBump.m"
+//#import "CSPBump.m"
 
 @implementation CSPBoardView
 {
-
-
+    
+    
 }
 const int BOARD_SIZE = 500;
 const int GRID_SQUARES = 19;
@@ -24,38 +24,50 @@ NSPoint endPoint;
 {
     self = [super initWithFrame:frame];
     if (self) {
+        CSPBump* tempBump;
         /*
-        JPBRect = NSMakeRect(0, 0, 500, 500);
-        CSPBoardViewJPB = [[NSImageView alloc] initWithFrame:JPBRect];
-        [CSPBoardViewJPB setImageScaling:NSScaleToFit];
-        [CSPBoardViewJPB setImage:[NSImage imageNamed:@"Pente_Board_2.png"]];
-        [self addSubview:CSPBoardViewJPB];
+         JPBRect = NSMakeRect(0, 0, 500, 500);
+         CSPBoardViewJPB = [[NSImageView alloc] initWithFrame:JPBRect];
+         [CSPBoardViewJPB setImageScaling:NSScaleToFit];
+         [CSPBoardViewJPB setImage:[NSImage imageNamed:@"Pente_Board_2.png"]];
+         [self addSubview:CSPBoardViewJPB];
          */
         //[self addSubview:CSPBoardViewJPB positioned:NSWindowBelow relativeTo:CSPBoardViewJPB];
-                
+        [self setImage:[NSImage imageNamed:@"Pente_Board_2.png"]];
+        JPBRect = NSMakeRect(0, 0, GRID_SIZE, GRID_SIZE);
+        tempBump = [[CSPBump alloc] initWithFrame:JPBRect];
+        [tempBump setBlackStone];
+        [self addSubview:(tempBump)];
+        NSLog(@"x %f y %f W %f H %f\n", [tempBump frame].origin.x,
+              [tempBump frame].origin.y, [tempBump frame].size.width,
+              [tempBump frame].size.height);
+              
+        
+        
         for (int i=GRID_SIZE/2; i<BOARD_SIZE-GRID_SIZE; i+=GRID_SIZE)
         {
             for (int p=GRID_SIZE/2; p<BOARD_SIZE-GRID_SIZE; p+=GRID_SIZE){
                 
-/*
+                /*
+                 JPBRect = NSMakeRect(i, p, GRID_SIZE, GRID_SIZE);
+                 CSPBoardViewJPB2 = [[NSImageView alloc] initWithFrame:JPBRect];
+                 [CSPBoardViewJPB2 setImageScaling:NSScaleToFit];
+                 [CSPBoardViewJPB2 setImage:[NSImage imageNamed:@"Pente_Blank_Bump.png"]];
+                 [self addSubview:CSPBoardViewJPB2];
+                 */
+                // JPBRect = NSMakeRect(i, p, GRID_SIZE, GRID_SIZE);
+                //somehow call dauris's CSPBump instead of the above stuff.
+                //NSImageView *anImage;
                 JPBRect = NSMakeRect(i, p, GRID_SIZE, GRID_SIZE);
-                CSPBoardViewJPB2 = [[NSImageView alloc] initWithFrame:JPBRect];
-                [CSPBoardViewJPB2 setImageScaling:NSScaleToFit];
-                [CSPBoardViewJPB2 setImage:[NSImage imageNamed:@"Pente_Blank_Bump.png"]];
-                [self addSubview:CSPBoardViewJPB2];
-  */
-            // JPBRect = NSMakeRect(i, p, GRID_SIZE, GRID_SIZE);
-            //somehow call dauris's CSPBump instead of the above stuff.
-            //NSImageView *anImage;
-            
-                CSPBoardViewJPB2 = [[CSPBump alloc] initWithFrame:frame];
-                [self addSubview:CSPBoardViewJPB2];
-             
-
+                tempBump = [[CSPBump alloc] initWithFrame:JPBRect];
+                [tempBump setBlankBump];
+                [self addSubview:tempBump];
+                NSLog(@"Placing bump %@ at %d, %d", tempBump, i, p);
+                
             }
             
         }
-
+        
     }
     return self;
 }
@@ -66,19 +78,20 @@ NSPoint endPoint;
 {
     
     [super drawRect:dirtyRect];
-    JPBRect = NSMakeRect(0, 0, 500, 500);
-    CSPBoardViewJPB = [[NSImageView alloc] initWithFrame:JPBRect];
-    [CSPBoardViewJPB setImageScaling:NSScaleToFit];
-    [CSPBoardViewJPB setImage:[NSImage imageNamed:@"Pente_Board_2.png"]];
-    [self addSubview:CSPBoardViewJPB];
-    // [self addSubview:CSPBoardViewJPB positioned:NSWindowBelow relativeTo:CSPBoardViewJPB2];
-    
+    /*
+     JPBRect = NSMakeRect(0, 0, 500, 500);
+     CSPBoardViewJPB = [[NSImageView alloc] initWithFrame:JPBRect];
+     [CSPBoardViewJPB setImageScaling:NSScaleToFit];
+     [CSPBoardViewJPB setImage:[NSImage imageNamed:@"Pente_Board_2.png"]];
+     [self addSubview:CSPBoardViewJPB];
+     // [self addSubview:CSPBoardViewJPB positioned:NSWindowBelow relativeTo:CSPBoardViewJPB2];
+     */
     
     NSBezierPath * path = [NSBezierPath bezierPath];
-  
     
     
-   
+    
+    
     //Sketch horizontal lines
     for (int i=0; i<BOARD_SIZE+GRID_SIZE; i+=GRID_SIZE)
     {
@@ -99,14 +112,14 @@ NSPoint endPoint;
         [path  moveToPoint: startPoint];
         [path lineToPoint:endPoint];
     }
-
+    
     //Ink the sketch
     [[NSColor blackColor] set];
     [path setLineWidth: 2];
     [path stroke];
     //[self addSubview: CSPBoardViewJPB];
     // [self addSubview:CSPBoardViewJPB2];
-   
+    
 }
 
 @end
