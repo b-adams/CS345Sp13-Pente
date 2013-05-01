3a1b3863ef61bfa747a0b26e4c18f9842556f234
Keegan Evans <kevans@Mac110-Selesnya.local>
2013-05-01 10:27:39 -0400

    Last Changes for Drag sources

--- CS345 Pente/CS345 Pente/CSPBoardViewTest.h
+++ CS345 Pente/CS345 Pente/CSPBoardViewTest.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Wells CS345: GUI programming. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import "CSPBowlView.h"
 
 @interface CSPBoardViewTest : CSPBowlView
 
--- CS345 Pente/CS345 Pente/CSPBoardViewTest.m
+++ CS345 Pente/CS345 Pente/CSPBoardViewTest.m
@@ -7,6 +7,7 @@
 //
 
 #import "CSPBoardViewTest.h"
+#import "CSPBowlView.h"
 
 @implementation CSPBoardViewTest
 
@@ -15,9 +16,36 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code here.
+        [self setImage: [NSImage imageNamed:@"Pente_Blank_Bump.png"]];
     }
     
     return self;
+}
+-(void)drawBumps
+{
+    const int BOARD_SIZE = 500;
+    const int GRID_SQUARES = 19;
+    const int GRID_SIZE = BOARD_SIZE/(GRID_SQUARES+1);
+    
+    NSPoint startPoint;
+    NSPoint endPoint;
+    
+    for (int i=0; i<BOARD_SIZE+GRID_SIZE; i+=GRID_SIZE)
+    {
+        startPoint = NSMakePoint(0, i);
+        [self addSubview:self];
+        endPoint   = NSMakePoint(BOARD_SIZE, i);
+    }
+    
+    //Sketch vertical lines
+    for (int i=0; i<BOARD_SIZE+GRID_SIZE; i+=GRID_SIZE)
+    {
+        startPoint = NSMakePoint(i, 0);
+        [self addSubview:self];
+
+        endPoint   = NSMakePoint(i, BOARD_SIZE);
+    }
+
 }
 
 -(void)awakeFromNib
@@ -34,6 +62,7 @@
     if((desiredAction & NSDragOperationCopy) != 0)
     {
         [self setImageFrameStyle:NSImageFramePhoto];
+        [self setImageFrameStyle:NSImageFrameGrayBezel];
         return NSDragOperationCopy;
     }
     //Oh well, not our flavor of drag
@@ -60,16 +89,17 @@
         dropAccepted = YES;
         if([[theColor lowercaseString] isEqualToString:@"white"])
         {
-            [self setToWhite];
+            [self setImage: [NSImage imageNamed:@"Pente_White_Stone.png"]];
         }
         else if([[theColor lowercaseString] isEqualToString:@"black"])
         {
-            [self setToBlack];
+            [self setImage: [NSImage imageNamed:@"Pente_Black Stone.png"]];
         }
         else
         {
             NSLog(@"Oh noes! I dunno how to handle '%@' data", theColor);
             dropAccepted=NO;
+            [self setImage: [NSImage imageNamed:@"Pente_Blank_Bump.png"]];
         }
     }
     
